638d4c98e25ea96528cf73f5f781d63e3b4669d4
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-12 14:51:29 -0700

    adding base route

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
-
-const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
     <Route path="/" component={Voting} />
 </Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting"/>,
-  document.getElementById('app')
+    <Router history={hashHistory}>{routes}</Router>,
+    document.getElementById('app')
 );
