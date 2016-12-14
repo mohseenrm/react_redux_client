ae5c5f5f94f52872ad4b3ea3efd35577928ddfd5
Mohseen Mukaddam <mohseenrm@users.noreply.github.com>
2016-12-14 12:32:08 -0700

    results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-
+//component needs update
 export default React.createClass({
     mixins: [PureRenderMixin],
     render: function() {
