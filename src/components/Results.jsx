1eb17d7a5f5ac15fe1cedf39854eeb1d04288f1a
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-15 16:47:06 -0700

    get votes

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,6 +6,12 @@ export default React.createClass({
     getPair: function() {
         return this.props.pair || [];
     },
+    getVotes: function(entry) {
+        if (this.props.tally && this.props.tally.has(entry)) {
+            return this.props.tally.get(entry);
+        }
+        return 0;
+    },
     render: function() {
         return <div className="results">
             {this.getPair().map(entry =>
