feb099d6538280a0dd627f323ab493120a0344f5
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-15 16:38:13 -0700

    adding result feature

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,7 +3,16 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 //component needs update
 export default React.createClass({
     mixins: [PureRenderMixin],
+    getPair: function() {
+        return this.props.pair || [];
+    },
     render: function() {
-        return <div>Hello from results!</div>
+        return <div className="results">
+            {this.getPair().map(entry =>
+                <div key={entry} className="entry">
+                <h1>{entry}</h1>
+                </div>
+            )}
+            </div>;
     }
 });
