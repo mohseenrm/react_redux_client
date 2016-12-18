8d010ac845ad0d5a634ea46d78ff83b668c3fa80
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-18 11:54:26 -0700

    undefined unit test

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -60,4 +60,28 @@ describe('reducer', () => {
             }
         }));
     });
+
+    it('handles SET_STATE without initial state', () => {
+        const action = {
+            type: 'SET_STATE',
+            state: {
+                vote: {
+                    pair: ['Trainspotting', '28 Days Later'],
+                    tally: {
+                        Trainspotting: 1
+                    }
+                }
+            }
+        };
+        const nextState = reducer(undefined, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days Later'],
+                tally: {
+                    Trainspotting: 1
+                }
+            }
+        }));
+    });
 });
