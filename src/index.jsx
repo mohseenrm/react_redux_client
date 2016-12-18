8148e082ed8b6a31b89e58dcb17d98b618fa3661
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-18 12:09:26 -0700

    adding reducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
 
 const routes = <Route component={App}>
-    <Route path="/results" component={Results} />
-    <Route path="/" component={Voting} />
+    <Route path="/results" component={Results}/>
+    <Route path="/" component={Voting}/>
 </Route>;
 
 ReactDOM.render(
-    <Router history={hashHistory}>{routes}</Router>,
-    document.getElementById('app')
-);
+    <Router history={hashHistory}>{routes}</Router>, document.getElementById('app'));
