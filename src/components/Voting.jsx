2c64253de9d7457da45d1f7495d9428b71e4afa9
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-18 12:21:53 -0700

    voting update

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import Winner from './Winner';
+import {connect} from 'react-redux';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Vote from './Vote';
 
-export default React.createClass({
+const Voting = React.createClass({
     mixins: [PureRenderMixin],
-    render: function() {
+    render: function () {
         return <div>
-        {this.props.winner ?
-            <Winner ref="winner" winner={this.props.winner} /> :
-            <Vote {...this.props} />}
+            {this.props.winner
+                ? <Winner ref="winner" winner={this.props.winner}/>
+                : <Vote {...this.props}/>}
         </div>;
     }
 });
+
+function mapStateToProps(state) {
+    return {
+        pair: state.getIn(['vote', 'pair']),
+        winner: state.get('winner')
+    };
+}
+
+connect(mapStateToProps)(Voting);
+
+export default Voting;
