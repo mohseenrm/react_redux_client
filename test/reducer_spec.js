033cbcd45e634662139c20dbc989dc5350049351
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-18 11:52:29 -0700

    immutable test

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -35,4 +35,29 @@ describe('reducer', () => {
             }
         }));
     });
+
+    it('handles SET_STATE with plain JS payload', () => {
+        const initialState = Map();
+        const action = {
+            type: 'SET_STATE',
+            state: {
+                vote: {
+                    pair: ['Trainspotting', '28 Days Later'],
+                    tally: {
+                        Trainspotting: 1
+                    }
+                }
+            }
+        };
+        const nextState = reducer(initialState, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days Later'],
+                tally: {
+                    Trainspotting: 1
+                }
+            }
+        }));
+    });
 });
