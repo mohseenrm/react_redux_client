e8fd454e16646e8fdab1b519b92bf23785613a1e
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-18 12:10:00 -0700

    adding store

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,21 @@ import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
 
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: [
+                'Sunshine', '28 Days Later'
+            ],
+            tally: {
+                Sunshine: 2
+            }
+        }
+    }
+});
+
 const routes = <Route component={App}>
     <Route path="/results" component={Results}/>
     <Route path="/" component={Voting}/>
