08721280c005e5f7abf409ccfc977a48dc74a1d0
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-19 01:59:30 -0700

    adding sample

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
-
+//sample
 const store = createStore(reducer);
 store.dispatch({
     type: 'SET_STATE',
